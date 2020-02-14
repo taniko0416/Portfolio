edbc116bfc0f7fa8d97842288b558dd275f5d6dc
新津康平 <niitsukouhei@niitsukouheinoMacBook-Air.local>
2020-02-14 17:29:58 +0900

    tmp commit

---
+++ webapp/webpack.config.prod.js
@@ -0,0 +1,74 @@
+/** TODO
+    - imitate liiga gulp
+    - clean package.json
+*/
+const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const ManifestPlugin = require('webpack-manifest-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    user: './app/frontend/js/user.jsx'
+  },
+  output: {
+    path: path.join(__dirname, 'app/javascripts/build/'),
+    filename: '[name].js',
+    publicPath: '/static/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      },
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: { plugins: [ autoprefixer ] },
+          },
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.(png|jpg|woff|woff2)$/,
+        loader: 'url-loader',
+        options: { limit: 8 * 1024 }
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file-loader?name=assets/[name].[hash].[ext]'
+      },
+    ]
+  },
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.esm.js',
+      OurCommonPath: path.join(__dirname, 'app/frontend/js/common'),
+      ScssVariables: path.join(__dirname, 'app/frontend/css/variables')
+    },
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new VueLoaderPlugin(),
+    // new ManifestPlugin(),
+  ],
+  performance: {
+    hints: false
+  },
+};
