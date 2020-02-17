7aab716609081d33bb9f77fbae4f43048b5766aa
新津康平 <niitsukouhei@niitsukouheinoMacBook-Air.local>
2020-02-17 23:50:46 +0900

    webpackによりできたJSファイルをコンパイル

--- webapp/config/initializers/assets.rb
+++ webapp/config/initializers/assets.rb
@@ -1,8 +1,1 @@
-# webpack_manifest_path = Rails.root.join('app', 'assets', 'manifest.json')
-# Rails.application.config.assets.webpack_manifest =
-#  if File.exist?(webpack_manifest_path)
-#    JSON.parse(File.read(webpack_manifest_path))
-#  end
-# Rails.application.config.assets.precompile += %w( build/user.js )
-# Rails.application.config.assets.precompile += %w( user.js )
-Rails.application.config.assets.precompile += %w(*.js *.css)
+Rails.application.config.assets.precompile += %w( user.js )
