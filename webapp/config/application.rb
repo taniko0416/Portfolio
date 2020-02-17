eaa0eadb84dcdb237de1ea9a131fcb28394b99ab
新津康平 <niitsukouhei@niitsukouheinoMacBook-Air.local>
2020-02-17 23:53:16 +0900

    コメント文を追加

--- webapp/config/application.rb
+++ webapp/config/application.rb
@@ -28,6 +28,8 @@ module Webapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    # 指定ドメインの流入を許可
     config.hosts << '.our-portfolio.net'
   end
 end
