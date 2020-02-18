47ca679177dcfffeb91b97f2a375b9984d965bac
新津康平 <niitsukouhei@niitsukouheinoMacBook-Air.local>
2020-02-18 19:30:21 +0900

    コンパイル

--- webapp/config/initializers/assets.rb
+++ webapp/config/initializers/assets.rb
@@ -1,4 +1,4 @@
 Rails.application.config.assets.precompile += %w( user.js )
 
 # 全てを読み込みたいときは以下を
-# Rails.application.config.assets.precompile += %w(*.js *.css)
+Rails.application.config.assets.precompile += %w(*.js *.css)
