e62cebf6682b770fa6cada6df439adebf6e00c6e
新津康平 <niitsukouhei@niitsukouheinoMacBook-Air.local>
2020-03-02 16:57:22 +0900

    headerを未ログインとログイン時で分けた

--- webapp/app/frontend/js/layouts/components/Navbar_login.vue
+++ webapp/app/frontend/js/layouts/components/Navbar_login.vue
@@ -8,19 +8,18 @@
     </template>
 
     <template slot="start">
-      <!-- <b-navbar-item href="#">Home</b-navbar-item>
-      <b-navbar-item href="#">Documentation</b-navbar-item>
-      <b-navbar-dropdown label="Info">
-        <b-navbar-item href="#">About</b-navbar-item>
-        <b-navbar-item href="#">Contact</b-navbar-item> -->
+      <!-- <b-navbar-item href="#">Home</b-navbar-item> -->
+      <!-- <b-navbar-item href="#">Documentation</b-navbar-item> -->
+      <!-- <b-navbar-dropdown label="Info"> -->
+        <!-- <b-navbar-item href="#">About</b-navbar-item> -->
+        <!-- <b-navbar-item href="#">Contact</b-navbar-item> -->
       </b-navbar-dropdown>
     </template>
 
     <template slot="end">
       <b-navbar-item tag="div">
         <div class="buttons">
-          <a href="/users/sign_up" class="button is-primary"><strong>Sign up</strong></a>
-          <a href="/users/sign_in" class="button is-light">Log in</a>
+          <a href="/users/sign_out" class="button is-light">Log out</a>
         </div>
       </b-navbar-item>
     </template>
--- webapp/app/frontend/js/user.jsx
+++ webapp/app/frontend/js/user.jsx
@@ -2,6 +2,7 @@ require('../css/user.scss');
 
 require('./layouts/index.js');
 require('./layouts/navbar.jsx');
+require('./layouts/navbar_login.jsx');
 require('./layouts/sidemenu.jsx');
 
 require('./member/main.jsx');
